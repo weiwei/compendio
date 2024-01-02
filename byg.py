1cee191d7bd5ea09e011fec271a9d1d8036fd91a
Weiwei <gastlygem@gmail.com>
2024-01-02 15:17:11 +0800

    fix zero date

--- byg.py
+++ byg.py
@@ -1,6 +1,8 @@
 import os
+from datetime import date
+
 import requests
-from utils import get_days_left
+from dateutil import relativedelta
 
 LOGIN_URL = "https://www.bygcloud.com/api/v1/passport/auth/login"
 INFO_URL = "https://www.bygcloud.com/api/v1/user/getSubscribe"
@@ -20,6 +22,9 @@ def get_usage():
     total = data["transfer_enable"]/1024/1024/1024
     used = (data["u"] + data["d"])/1024/1024/1024
     reset_day = data["reset_day"]
+    if reset_day == 0:
+        delta = relativedelta.relativedelta(months=1)
+        reset_day = date.today() + delta - date.today()
     return total, used, reset_day
 
 if __name__ == "__main__":
