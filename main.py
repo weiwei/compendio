8c4e063d3aac08dece4a51a5d00de89c5d5b25a7
Joel Wang <wangjo@jmac.ads.autodesk.com>
2024-01-03 14:23:40 +0800

    make mention work

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ total2, used2, days_left2 = byg.get_usage()
 post += f"BYG 月度使用流量: {used2:.2f} GB / {total2:.0f} GB\n"
 post += f"BYG 距月度重置日: {days_left2} 天\n"
 
+facets = [{
+    "index": {
+        "byteStart": 8,
+        "byteEnd": 27
+    },
+    "features": {
+        "$type": "app.bsky.richtext.facet#mention",
+        "did": "did:plc:vkrqsz2gz6lhorh52u7luijl"
+    }
+}]
+
 client = Client()
 client.login('compendio.bsky.social', os.environ['BSKY_PASSWORD'])
-client.send_post(text=post)
+client.send_post(text=post, facets=facets)
